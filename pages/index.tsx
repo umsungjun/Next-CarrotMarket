3b7576f332a4ccfb50d83cec181cc3a9ca0675e2
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-11-30 21:23:53 +0900

    Test Drive part Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,30 @@ export default function Home() {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl "></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl "></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl "></div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl ">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-semibold">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-semibold">$2,310</span>
+            </div>
+          </div>
+          <div className="relative -mt-10 -mb-5 flex flex-col items-center">
+            <span className="text-lg font-bold">Tony Molly</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-3xl shadow-xl "></div>
+      <div className="bg-white p-6 rounded-3xl shadow-xl "></div>
     </div>
   );
 }
