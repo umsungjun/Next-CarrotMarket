61bd0f5bc5939952e6eaab6c850d3c3909e9978b
umsungjun <umseongjun@naver.com>
2023-12-05 20:24:23 +0900

    Transitions
    Transitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,9 +60,9 @@ export default function Home() {
             </div>
             <div className="w-full flex justify-between">
               <div className="space-x-2">
-                <button className="w-5 h-5 rounded-full bg-yellow-500 ring-2 ring-offset-2" />
-                <button className="w-5 h-5 rounded-full bg-indigo-500" />
-                <button className="w-5 h-5 rounded-full bg-teal-500" />
+                <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
+                <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
+                <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2  ring-teal-500 transition" />
               </div>
               <div className="flex items-center gap-2">
                 <button className=" bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium  text-gray-400 rounded-lg">
