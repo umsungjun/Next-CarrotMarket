3fb1aee6fcb764d891b277475b32ec9add0a3056
umsungjun <umseongjun@naver.com>
2023-12-06 22:04:37 +0900

    modify map
    modify map

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,19 @@ export default function Home() {
     <div className="bg-slate-400 py-20 px-10 grid gap-10">
       <div className="bg-white p-6 rounded-3xl shadow-xl ">
         <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between my-2">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Tooly Table</span>
-          <span className="font-semibold">$19</span>
-        </div>
+        <ul>
+          {[1, 2, 3, 4, 5].map((i) => (
+            <div key={i} className="flex justify-between my-2">
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
+        {["a", "b", "c", ""].map((c, i) => (
+          <li className="bg-red-200 py-2 empty:hidden" key={i}>
+            {c}
+          </li>
+        ))}
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$10</span>
