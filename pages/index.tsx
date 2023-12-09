b133d466500a339f7652d30edfaf1b22f1ae96ac
umsungjun <umseongjun@naver.com>
2023-12-09 22:24:16 +0900

    Modifiers for Forms
    Modifiers for Forms

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="bg-white overflow-hidden rounded-3xl shadow-xl ">
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
         <div className="bg-blue-500 p-6 pb-14">
           <span className="text-white text-2xl">Profile</span>
         </div>
@@ -36,7 +36,7 @@ export default function Home() {
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-semibold">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-blue-400 transition-colors" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-semibold">$2,310</span>
