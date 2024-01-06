e8372a60117caa5610b7ee5eb0790f07619531f4
umsungjun <umseongjun@naver.com>
2024-01-06 23:02:02 +0900

    동네 글 쓰기 한국어 번들 완료
    동네 글 쓰기 한국어 번들 완료

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -3,15 +3,15 @@ import type { NextPage } from "next";
 
 const Write: NextPage = () => {
   return (
-    <Layout canGoBack title="동네 글 쓰기">
+    <Layout canGoBack title="동네 글쓰기">
       <form className="px-4 ">
         <textarea
           className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 resize-none"
           rows={4}
-          placeholder="Ask a question!"
+          placeholder="최신 동네의 이슈를 적어보세요!"
         />
         <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-          Submit
+          동네 글 등록
         </button>
       </form>
     </Layout>
