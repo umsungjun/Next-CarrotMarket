3a3bdc168019d3db31eddc921333cd851f2f1c7c
umsungjun <umseongjun@naver.com>
2024-01-14 17:42:23 +0900

    react-hook-form error handling test
    react-hook-form error handling test

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,23 +1,61 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  userName: string;
+  email: string;
+  password: string;
+}
 
 function Forms() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
+
+  const onValid = (data: LoginForm) => {
+    console.log("I'am Valid");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+  console.log(errors);
   return (
-    <form className="flex flex-col">
+    <form className="flex flex-col" onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("userName")}
+        {...register("userName", {
+          required: "UserName is required",
+          minLength: {
+            message: "The UserName should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="User Name"
-        required
+        className={`${errors.userName ? "border-red-500" : ""}`}
       />
-      <input {...register("email")} type="eamil" placeholder="Email" required />
+      {errors.userName?.message}
       <input
-        {...register("password")}
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("gmail.com") || "Gmail is not allowed",
+          },
+        })}
+        type="email"
+        placeholder="Email"
+      />
+      {errors.email?.message}
+      <input
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
-        required
       />
+      {errors.password?.message}
       <input type="submit" value="Create" />
     </form>
   );
