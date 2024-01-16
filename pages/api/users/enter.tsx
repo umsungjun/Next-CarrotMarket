a6b692934b3c783313754d89d2cef212f6d15ff3
umsungjun <umseongjun@naver.com>
2024-01-16 21:24:01 +0900

    백엔드 회원 생성 테스트 method 예외처리
    백엔드 회원 생성 테스트 method 예외처리

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,13 +5,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await client.user.create({
-    data: {
-      email: "hi",
-      name: "hi",
-    },
-  });
-  res.json({
-    ok: true,
-  });
+  if (req.method !== "POST") {
+    res.status(400).end;
+  }
+  console.log(req.body.email);
+  res.status(200).end();
 }
