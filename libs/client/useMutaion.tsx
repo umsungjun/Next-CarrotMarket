97bb91527a54761c2118f572234cd9d2bdfdc1ea
umsungjun <umseongjun@naver.com>
2024-01-18 21:18:22 +0900

    back-end API 호출 시 method예외 처리, 함수로 return
    back-end API 호출 시 method예외 처리, 함수로 return

--- libs/client/useMutaion.tsx
+++ libs/client/useMutaion.tsx
@@ -1,15 +1,43 @@
 import { useState } from "react";
 
-function useMutation(
-  url: string
-): [
-  (data: any) => void,
-  { loading: boolean; data: undefined | any; error: undefined | any }
-] {
+interface UseMutationState {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
+
+type UseMutationType = [(data: any) => void, UseMutationState];
+
+function useMutation(url: string): UseMutationType {
+  const [state, setState] = useState({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);
-  function mutaition(data: any) {}
+
+  function mutaition(data: any) {
+    setLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) =>
+        response
+          .json()
+          .catch(() => {})
+          .then((json) => setData(json))
+      )
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }
+
   return [mutaition, { loading, data, error }];
 }
+
 export default useMutation;
