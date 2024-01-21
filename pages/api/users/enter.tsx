842e41dcc179521962216a9368bae32549ace48d
umsungjun <umseongjun@naver.com>
2024-01-21 16:32:52 +0900

    client.user.upsert활용해서 DB에 회원이 존재하는지 안하는지 확인하고 생성
    client.user.upsert활용해서 DB에 회원이 존재하는지 안하는지 확인하고 생성

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,19 @@ import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload, // email : email 형식으로 보내야 하기 때문에 ...을 활용
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
