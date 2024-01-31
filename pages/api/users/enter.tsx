9f7d0cac24b621f5f0c4c8fa5fad53541c0be9b1
umsungjun <umseongjun@naver.com>
2024-01-31 20:39:29 +0900

    Update enter.tsx

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -35,29 +35,23 @@ async function handler(
     },
   });
   if (phone) {
-    const message = await twilioClient.messages.create({
+    const phoneRes = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_MSID,
       to: process.env.MY_PHONE!, // 이론상에는 가입한 번호에 문자 메시지가 가는게 맞지만 twilio가 평가판 계정이기 때문에 내 핸드폰 번호로 문자 전송 테스트
       body: `인증번호: ${payload}. `,
     });
-    console.log(message);
+    console.log("phoneRes", phoneRes);
   }
   if (email) {
     const mailOptions = {
       from: process.env.MY_EMAIL,
       to: email,
-      subject: "당근 마켓",
+      subject: "당근 마켓 인증 메일",
       text: `인증번호 : ${payload}`,
     };
-    const result = await smtpTransport.sendMail(
-      mailOptions,
-      (error, responses) => {
-        if (error) {
-          console.error(error);
-        }
-      }
-    );
+    const emailRes = await smtpTransport.sendMail(mailOptions);
     smtpTransport.close();
+    console.log("emailRes", emailRes);
   }
   return res.json({
     ok: true,
