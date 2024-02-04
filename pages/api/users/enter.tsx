8ed1461ea350c76e5e49891c89a91fc11c7a75c1
umsungjun <umseongjun@naver.com>
2024-02-04 15:22:51 +0900

    인증번호 6자리로 변경
    인증번호 6자리로 변경

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -16,7 +16,7 @@ async function handler(
   const { phone, email } = req.body;
   const user = phone ? { phone } : email ? { email } : null;
   if (!user) return res.status(400).json({ ok: false });
-  const payload = Math.floor(100000 * Math.random() * 90000) + "";
+  const payload = Math.floor(100000 * Math.random() * 99) + "";
 
   const token = await client.token.create({
     data: {
