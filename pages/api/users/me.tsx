d4b64424fec7f5d3ae804f3993ebcc2b400dad7f
umsungjun <umseongjun@naver.com>
2024-02-07 21:29:13 +0900

    me api private user 예외처리
    me api private user 예외처리

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -15,4 +15,4 @@ async function handler(
   res.json({ ok: true, profile });
 }
 
-export default withApiSession(withHandler("GET", handler));
+export default withApiSession(withHandler({ method: "GET", handler }));
