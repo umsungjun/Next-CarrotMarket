7394993360804b5b047504c8a8234c742e32177d
umsungjun <umseongjun@naver.com>
2024-02-14 21:23:37 +0900

    uploadForm 생성
    uploadForm 생성

--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -2,44 +2,69 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import Layout from "@/components/layout";
 import TextArea from "@/components/textarea";
+import useMutation from "@/libs/client/useMutaion";
 import type { NextPage } from "next";
+import { useForm } from "react-hook-form";
+
+interface UploadProductForm {
+  name: string;
+  price: number;
+  description: string;
+}
 
 const Upload: NextPage = () => {
+  const { register, handleSubmit } = useForm<UploadProductForm>();
+  const [upLoadProduct, { loading, data, error }] = useMutation("api/products");
+  const onValid = (data: UploadProductForm) => {
+    if (loading) return;
+    console.log(data);
+  };
+
   return (
     <Layout canGoBack title="상품 등록">
-      <div className="px-4 space-y-5 ">
+      <form className="px-4 space-y-5" onSubmit={handleSubmit(onValid)}>
         <div>
-          <div>
-            <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
-              <svg
-                className="h-12 w-12"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 48 48"
-                aria-hidden="true"
-              >
-                <path
-                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <input className="hidden" type="file" />
-            </label>
-          </div>
-        </div>
-        <div>
-          <Input label="가격" name="price" kind="price" />
-        </div>
-        <div>
-          <label className="mb-1 block text-sm font-medium text-gray-700">
-            설명
+          <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
+            <svg
+              className="h-12 w-12"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <input className="hidden" type="file" />
           </label>
-          <TextArea />
         </div>
-        <Button text="상품 등록" />
-      </div>
+        <Input
+          register={register("name", { required: true })}
+          label="이름"
+          name="name"
+          kind="text"
+          type="text"
+          required
+        />
+        <Input
+          register={register("price", { required: true })}
+          label="가격"
+          name="price"
+          kind="price"
+          type="number"
+          required
+        />
+        <TextArea
+          register={register("description", { required: true })}
+          name="Description"
+          label="설명"
+        />
+        <Button text={loading ? "상품 등록 중..." : "상품 등록"} />
+      </form>
     </Layout>
   );
 };
