7c951590ae881cf16525f452bbab8f13e69f6a16
umsungjun <umseongjun@naver.com>
2024-02-27 21:39:04 +0900

    api/proudcts 복구
    api/proudcts 복구

---
+++ pages/api/products/index.ts
@@ -0,0 +1,39 @@
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  if (req.method === "GET") {
+    const products = await client.product.findMany({});
+    res.json({ ok: true, products });
+  }
+  if (req.method === "POST") {
+    const {
+      body: { name, price, description },
+      session: { user },
+    } = req;
+    const product = await client.product.create({
+      data: {
+        image:
+          "https://cdn.motor1.com/images/mgl/zxQBp4/s3/2024-lamborghini-revuelto-exterior.jpg",
+        name,
+        price,
+        description,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({ ok: true, product });
+  }
+}
+
+export default withApiSession(
+  withHandler({ methods: ["GET", "POST"], handler })
+);
