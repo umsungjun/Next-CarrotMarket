7c82a5541ca7468f63e59e32cea1e44f36a1f751
umsungjun <umseongjun@naver.com>
2024-03-03 00:01:56 +0900

    동네 글 작성 기능 구현
    동네 글 작성 기능 구현

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,14 +1,46 @@
 import Button from "@/components/button";
 import Layout from "@/components/layout";
 import TextArea from "@/components/textarea";
+import useMutation from "@/libs/client/useMutation";
+import { Post } from "@prisma/client";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [post, { loading, data, error }] =
+    useMutation<WriteResponse>("/api/posts");
+  const onValid = (data: WriteForm) => {
+    if (loading) return;
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
+
   return (
     <Layout canGoBack title="동네 글쓰기">
-      <form className="px-4 ">
-        <TextArea />
-        <Button text="동네 글 등록" />
+      <form className="px-4" onSubmit={handleSubmit(onValid)}>
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="질문을 입력해주세요!"
+        />
+        <Button text={loading ? "글 등록중..." : "동네 글 등록"} />
       </form>
     </Layout>
   );
