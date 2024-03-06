711520b7eff263861b8013b6c40c35ced6083d13
umsungjun <umseongjun@naver.com>
2024-03-06 21:34:42 +0900

    post/answer API 구현
    post/answer API 구현

---
+++ pages/api/posts/[id]/answer.ts
@@ -0,0 +1,46 @@
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+    body: { answer },
+  } = req;
+  const post = await client.post.findUnique({
+    where: {
+      id: +id!.toString(),
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (!post) {
+    res.json({ ok: false });
+  }
+
+  const newAnswer = await client.answer.create({
+    data: {
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+      post: {
+        connect: {
+          id: +id!.toString(),
+        },
+      },
+      answer,
+    },
+  });
+  console.log(newAnswer);
+  res.json({ ok: true, answer: newAnswer });
+}
+
+export default withApiSession(withHandler({ methods: ["POST"], handler }));
