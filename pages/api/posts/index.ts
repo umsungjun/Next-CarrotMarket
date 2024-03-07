5070540dc999c8f57f8dc7c87c97a1edb1e3823e
umsungjun <umseongjun@naver.com>
2024-03-07 21:41:30 +0900

    Get Posts API 구현
    Get Posts API 구현

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -11,18 +11,42 @@ async function handler(
     body: { question },
     session: { user },
   } = req;
-  const post = await client.post.create({
-    data: {
-      question,
-      user: {
-        connect: {
-          id: user?.id,
+  if (req.method === "GET") {
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wonderings: true,
+            answers: true,
+          },
         },
       },
-    },
-  });
+    });
+    res.json({ ok: true, posts });
+  }
 
-  res.json({ ok: true, post });
+  if (req.method === "POST") {
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({ ok: true, post });
+  }
 }
 
-export default withApiSession(withHandler({ methods: ["POST"], handler }));
+export default withApiSession(
+  withHandler({ methods: ["GET", "POST"], handler })
+);
