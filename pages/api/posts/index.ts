ba63c6a834adfeea549ad6469af07ee0f1e671e0
umsungjun <umseongjun@naver.com>
2024-03-08 20:46:18 +0900

    Feat Create Post data 추가
    latitude,  longitude

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -8,7 +8,7 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const {
-    body: { question },
+    body: { question, latitude, longitude },
     session: { user },
   } = req;
   if (req.method === "GET") {
@@ -36,6 +36,8 @@ async function handler(
     const post = await client.post.create({
       data: {
         question,
+        latitude,
+        longitude,
         user: {
           connect: {
             id: user?.id,
