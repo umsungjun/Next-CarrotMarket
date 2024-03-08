266d55cb5d5545b5c3d407620de3294a8bf39a73
umsungjun <umseongjun@naver.com>
2024-03-08 20:45:29 +0900

    Feat req body 데이터 추가
    latitude, longitude

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/button";
 import Layout from "@/components/layout";
 import TextArea from "@/components/textarea";
+import useCoords from "@/libs/client/useCoords";
 import useMutation from "@/libs/client/useMutation";
 import { Post } from "@prisma/client";
 import type { NextPage } from "next";
@@ -18,13 +19,14 @@ interface WriteResponse {
 }
 
 const Write: NextPage = () => {
+  const { latitude, longitude } = useCoords();
   const router = useRouter();
   const { register, handleSubmit } = useForm<WriteForm>();
   const [post, { loading, data, error }] =
     useMutation<WriteResponse>("/api/posts");
   const onValid = (data: WriteForm) => {
     if (loading) return;
-    post(data);
+    post({ ...data, latitude, longitude });
   };
   useEffect(() => {
     if (data && data.ok) {
