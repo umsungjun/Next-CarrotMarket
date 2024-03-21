8bc06b699d072e5f63c0273b1a933638106d6657
umsungjun <umseongjun@naver.com>
2024-03-21 21:55:52 +0900

    Feat: Profile Edit 기능 구현
    Create POST Profile Edit 백엔드 API

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -7,12 +7,91 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const profile = await client.user.findUnique({
-    where: {
-      id: req.session.user?.id,
-    },
-  });
-  res.json({ ok: true, profile });
+  if (req.method === "GET") {
+    const profile = await client.user.findUnique({
+      where: {
+        id: req.session.user?.id,
+      },
+    });
+    res.json({ ok: true, profile });
+  }
+
+  if (req.method === "POST") {
+    const {
+      session: { user },
+      body: { name, email, phone },
+    } = req;
+    const currentUser = await client.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+    });
+
+    if (email && currentUser?.email !== email) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            email,
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({ ok: false, error: "이미 사용중인 이메일 입니다." });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          email,
+        },
+      });
+      return res.json({ ok: true });
+    }
+    if (phone && currentUser?.phone !== phone) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            phone,
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: "이미 사용중인 휴대폰 번호 입니다.",
+        });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          phone,
+        },
+      });
+      return res.json({ ok: true });
+    }
+    if (name) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          name,
+        },
+      });
+    }
+    return res.json({ ok: true });
+  }
 }
 
-export default withApiSession(withHandler({ methods: ["GET"], handler }));
+export default withApiSession(
+  withHandler({ methods: ["GET", "POST"], handler })
+);
