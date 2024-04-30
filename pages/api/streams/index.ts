d5ccdd076037b412cb8d1ffe46ce325ccaa16d14
umsungjun <umseongjun@naver.com>
2024-04-30 19:57:34 +0900

    Feat: Create Stream Post API

---
+++ pages/api/streams/index.ts
@@ -0,0 +1,33 @@
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+    body: { name, price, description },
+  } = req;
+  if (req.method === "POST") {
+    const stream = await client.stream.create({
+      data: {
+        name,
+        price,
+        description,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    return res.json({ ok: true, stream });
+  }
+}
+
+export default withApiSession(
+  withHandler({ methods: ["GET", "POST"], handler })
+);
