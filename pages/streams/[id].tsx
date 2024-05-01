48a6abad7c59951535ac4a6d842ca7f5910a9007
umsungjun <umseongjun@naver.com>
2024-05-01 20:25:44 +0900

    Feat: LiveStream 채팅 전송 기능 구현

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -1,19 +1,36 @@
 import Layout from "@/components/layout";
+import Message from "@/components/message";
+import useMutation from "@/libs/client/useMutation";
 import { Stream } from "@prisma/client";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 import useSWR from "swr";
 
 interface StreamResponse {
   ok: boolean;
   stream: Stream;
 }
+interface MessageForm {
+  message: string;
+}
 
-const LiveDetail: NextPage = () => {
+const StreamDetail: NextPage = () => {
   const router = useRouter();
+  const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data } = useSWR<StreamResponse>(
     router.query.id ? `/api/streams/${router.query.id}` : null
   );
+
+  const [sendMessage, { loading, data: sendMessageData }] = useMutation(
+    `/api/streams/${router.query.id}/messages`
+  );
+
+  const onValid = (form: MessageForm) => {
+    if (loading) return;
+    sendMessage(form);
+    reset();
+  };
 
   return (
     <Layout canGoBack>
@@ -28,78 +45,26 @@ const LiveDetail: NextPage = () => {
         <p className=" my-6 text-gray-700">{data?.stream?.description}</p>
         <h1 className="text-xl font-bold text-gray-900">Live Chat</h1>
         <div className="py-10 pb-16 h-[50vh] px-4 space-y-4 overflow-y-scroll">
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
-          <div className="flex items-start space-x-2">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>Hi how much are you selling them for?</p>
-            </div>
-          </div>
-          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-            <div className="w-8 h-8 rounded-full bg-slate-400" />
-            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-              <p>I want ￦20,000</p>
-            </div>
-          </div>
+          <Message message="hi" />
+          <Message message="hi" reversed />
+          <Message message="hi" />
           <div className="fixed w-full mx-auto max-w-md bottom-2 inset-x-0">
-            <div className="flex relative items-center">
+            <form
+              className="flex relative items-center"
+              onSubmit={handleSubmit(onValid)}
+            >
               <input
                 type="text"
-                className="shdow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none focus:border-orange-500 pr-12"
+                required
+                {...register("message", { required: true })}
+                className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none focus:border-orange-500 pr-12"
               />
               <div className="absolute inset-y-0 flex py-1.5 px-1.5 right-0">
                 <button className="flex items-center focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 bg-orange-500 hover:bg-orange-600 rounded-full px-3 text-sm text-white cursor-pointer">
                   &rarr;
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
@@ -107,4 +72,4 @@ const LiveDetail: NextPage = () => {
   );
 };
 
-export default LiveDetail;
+export default StreamDetail;
