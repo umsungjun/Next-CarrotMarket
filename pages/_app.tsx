020ec1e6392d915c27ab22f21e79a20c8bbf8721
umsungjun <umseongjun@naver.com>
2024-05-12 17:18:51 +0900

    Feat: 최대 너비 지정 및 , 테두리 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import useUser from "@/libs/client/useUser";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
@@ -11,9 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
           fetch(url).then((response) => response.json()),
       }}
     >
-      <div className="w-full max-w-xl mx-auto">
+      <div className="relative w-full max-w-[480px] mx-auto h-screen border border-y-0">
         <Component {...pageProps} />
       </div>
     </SWRConfig>
   );
 }
+``;
