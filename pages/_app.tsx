b48077cd4aaf793071b3459def1768e22d6726f6
umsungjun <umseongjun@naver.com>
2024-05-13 21:19:03 +0900

    Feat: Favicon 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,6 +12,15 @@ export default function App({ Component, pageProps }: AppProps) {
       }}
     >
       <div className="relative w-[480px] h-full mx-auto border border-y-0 box-content">
+        <Head>
+          <link
+            rel="icon"
+            href={
+              "https://imagedelivery.net/Fxbz5xV7vyEmqagr1Ejwow/ee3e812e-0841-4f78-5432-45848ea0e800/public"
+            }
+            sizes="any"
+          />
+        </Head>
         <Component {...pageProps} />
       </div>
     </SWRConfig>
