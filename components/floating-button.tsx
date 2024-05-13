a7f384c24cb8bdf53ee4d28c65831af65e39410b
umsungjun <umseongjun@naver.com>
2024-05-13 20:44:45 +0900

    Design: InnerWidth에 따른 포지션 변경
    innerWidth <= 450 ? "fixed" : "absolute"

--- components/floating-button.tsx
+++ components/floating-button.tsx
@@ -1,5 +1,6 @@
+import { cls } from "@/libs/client/utils";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface FloatingButton {
   children: React.ReactNode;
@@ -7,10 +8,28 @@ interface FloatingButton {
 }
 
 function FloatingButton({ children, href }: FloatingButton) {
+  const [innerWidth, setInnerWidth] = useState<number>(0);
+
+  const resizeListener = () => {
+    setInnerWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    resizeListener();
+    window.addEventListener("resize", resizeListener);
+
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    };
+  }, []);
+
   return (
     <Link
       href={href}
-      className="absolute  hover:bg-orange-500 border-0 aspect-square border-transparent transition-colors cursor-pointer bottom-24 right-4 shadow-xl bg-orange-400 rounded-full w-14 flex items-center justify-center text-white"
+      className={cls(
+        innerWidth <= 450 ? "fixed" : "absolute",
+        "hover:bg-orange-500 border-0 aspect-square border-transparent transition-colors cursor-pointer bottom-24 right-4 shadow-xl bg-orange-400 rounded-full w-14 flex items-center justify-center text-white"
+      )}
     >
       {children}
     </Link>
