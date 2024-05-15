c11cff5007149d588dc28c9221abb30fa3782cca
umsungjun <umseongjun@naver.com>
2024-05-15 13:36:54 +0900

    Feat: Page Title 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import FloatingButton from "@/components/floating-button";
 import Item from "@/components/item";
 import Layout from "@/components/layout";
+import Title from "@/components/title";
 import useUser from "@/libs/client/useUser";
 import { Product } from "@prisma/client";
-import Head from "next/head";
 import useSWR from "swr";
 
 export interface ProductWithCount extends Product {
@@ -22,41 +22,41 @@ function Home() {
   const { data } = useSWR<ProductsResponse>("/api/products");
 
   return (
-    <Layout title="홈" hasTabBar>
-      <Head>
-        <title>홈</title>
-      </Head>
-      <div className="flex flex-col space-y-5">
-        {data?.products?.map((product) => (
-          <Item
-            id={product.id}
-            key={product.id}
-            title={product.name}
-            price={product.price}
-            image={product.image}
-            comments={1}
-            hearts={product._count.favs}
-          />
-        ))}
-        <FloatingButton href="/products/upload">
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+    <>
+      <Title pageTitle={"홈"} />
+      <Layout title="홈" hasTabBar>
+        <div className="flex flex-col space-y-5">
+          {data?.products?.map((product) => (
+            <Item
+              id={product.id}
+              key={product.id}
+              title={product.name}
+              price={product.price}
+              image={product.image}
+              hearts={product._count.favs}
+              createdAt={product.createdAt}
             />
-          </svg>
-        </FloatingButton>
-      </div>
-    </Layout>
+          ))}
+          <FloatingButton href="/products/upload">
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </FloatingButton>
+        </div>
+      </Layout>
+    </>
   );
 }
 
